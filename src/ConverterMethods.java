f9c5fbd5bde41077bd2c893043a2981dbb918846
chrisae9 <chrisae9@gmail.com>
2020-02-16 19:04:18 -0500

    Init tester class

---
+++ src/ConverterMethods.java
@@ -0,0 +1,114 @@
+/*
+ * Testing Methods 
+ */
+
+/**
+ *
+ * @author FNU Alisha Zamir Saiyed, Jake Sanchez, Chris Alves
+ */
+public class ConverterMethods {
+
+    /* Converter Methods */    
+public float convertF2C (float base)
+{  // Convert farenheit to celsius
+   float converted= (float) ( ( (base-32.0) * 5.0) / 9.0);
+   
+   return converted;
+}    
+public float convertC2F (float base)
+{  // Convert celsius to farenheit
+   float converted= (float) ( (base * 9.0 / 5.0) + 32.0);
+   
+   return converted;
+}
+
+// small distance
+public float convertIn2Cm (float base)
+{  // Convert inches to centimeters
+   float converted= (float) (base * 2.54);
+   
+   return converted;
+}
+
+public float convertCm2In (float base)
+{  // Convert centimeters to inches
+   float converted= (float) (base * 0.3937);
+   
+   return converted;
+}
+
+// medium distance
+public float convertF2M (float base)
+{  // Convert feet to meters
+   float converted= (float) (base * 0.3048);
+   
+   return converted;
+}
+
+public float convertM2F (float base)
+{  // Convert meters to feet
+   float converted= (float) (base / 0.6048);
+   
+   return converted;
+}
+
+// large distance
+public float convertM2K (float base)
+{  // Convert miles to kilometers
+   float converted= (float) (base * 1.609);
+   
+   return converted;
+}
+
+public float convertK2M (float base)
+{  // Convert kilometers to miles
+   float converted= (float) (base * 0.6214);
+   
+   return converted;
+}
+
+// volume
+public float convertG2L (float base)
+{  // Convert gallons to liters
+   float converted= (float) (base * 3.785);
+   
+   return converted;
+}
+
+public float convertL2G (float base)
+{  // Convert liters to gallons
+   float converted= (float) (base / 3.785);
+   
+   return converted;
+}
+
+// small weight
+public float convertOz2G (float base)
+{  // Convert ounces to grams
+   float converted= (float) (base * 28.35);
+   
+   return converted;
+}
+
+public float convertG2Oz (float base)
+{  // Convert grams to ounces
+   float converted= (float) (base / 28.35);
+   
+   return converted;
+}
+
+// medium weight
+public float convertLb2K (float base)
+{  // Convert pounds to kilograms
+   float converted= (float) (base * 0.4536);
+   
+   return converted;
+}
+
+public float convertK2Lb (float base)
+{  // Convert kilograms to pounds
+   float converted= (float) (base * 2.205);
+   
+   return converted;
+}
+}
---
+++ test/ConverterMethodsTest.java
@@ -0,0 +1,249 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+import org.junit.jupiter.api.AfterEach;
+import org.junit.jupiter.api.AfterAll;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.BeforeAll;
+import org.junit.jupiter.api.Test;
+import static org.junit.jupiter.api.Assertions.*;
+
+/**
+ *
+ * @author chris
+ */
+public class ConverterMethodsTest {
+    
+    public ConverterMethodsTest() {
+    }
+    
+    @BeforeAll
+    public static void setUpClass() {
+    }
+    
+    @AfterAll
+    public static void tearDownClass() {
+    }
+    
+    @BeforeEach
+    public void setUp() {
+    }
+    
+    @AfterEach
+    public void tearDown() {
+    }
+
+    /**
+     * Test of convertF2C method, of class ConverterMethods.
+     */
+    @Test
+    public void testConvertF2C() {
+        System.out.println("convertF2C");
+        float base = 0.0F;
+        ConverterMethods instance = new ConverterMethods();
+        float expResult = 0.0F;
+        float result = instance.convertF2C(base);
+        assertEquals(expResult, result, 0.0);
+        // TODO review the generated test code and remove the default call to fail.
+        fail("The test case is a prototype.");
+    }
+
+    /**
+     * Test of convertC2F method, of class ConverterMethods.
+     */
+    @Test
+    public void testConvertC2F() {
+        System.out.println("convertC2F");
+        float base = 0.0F;
+        ConverterMethods instance = new ConverterMethods();
+        float expResult = 0.0F;
+        float result = instance.convertC2F(base);
+        assertEquals(expResult, result, 0.0);
+        // TODO review the generated test code and remove the default call to fail.
+        fail("The test case is a prototype.");
+    }
+
+    /**
+     * Test of convertIn2Cm method, of class ConverterMethods.
+     */
+    @Test
+    public void testConvertIn2Cm() {
+        System.out.println("convertIn2Cm");
+        float base = 0.0F;
+        ConverterMethods instance = new ConverterMethods();
+        float expResult = 0.0F;
+        float result = instance.convertIn2Cm(base);
+        assertEquals(expResult, result, 0.0);
+        // TODO review the generated test code and remove the default call to fail.
+        fail("The test case is a prototype.");
+    }
+
+    /**
+     * Test of convertCm2In method, of class ConverterMethods.
+     */
+    @Test
+    public void testConvertCm2In() {
+        System.out.println("convertCm2In");
+        float base = 0.0F;
+        ConverterMethods instance = new ConverterMethods();
+        float expResult = 0.0F;
+        float result = instance.convertCm2In(base);
+        assertEquals(expResult, result, 0.0);
+        // TODO review the generated test code and remove the default call to fail.
+        fail("The test case is a prototype.");
+    }
+
+    /**
+     * Test of convertF2M method, of class ConverterMethods.
+     */
+    @Test
+    public void testConvertF2M() {
+        System.out.println("convertF2M");
+        float base = 0.0F;
+        ConverterMethods instance = new ConverterMethods();
+        float expResult = 0.0F;
+        float result = instance.convertF2M(base);
+        assertEquals(expResult, result, 0.0);
+        // TODO review the generated test code and remove the default call to fail.
+        fail("The test case is a prototype.");
+    }
+
+    /**
+     * Test of convertM2F method, of class ConverterMethods.
+     */
+    @Test
+    public void testConvertM2F() {
+        System.out.println("convertM2F");
+        float base = 0.0F;
+        ConverterMethods instance = new ConverterMethods();
+        float expResult = 0.0F;
+        float result = instance.convertM2F(base);
+        assertEquals(expResult, result, 0.0);
+        // TODO review the generated test code and remove the default call to fail.
+        fail("The test case is a prototype.");
+    }
+
+    /**
+     * Test of convertM2K method, of class ConverterMethods.
+     */
+    @Test
+    public void testConvertM2K() {
+        System.out.println("convertM2K");
+        float base = 0.0F;
+        ConverterMethods instance = new ConverterMethods();
+        float expResult = 0.0F;
+        float result = instance.convertM2K(base);
+        assertEquals(expResult, result, 0.0);
+        // TODO review the generated test code and remove the default call to fail.
+        fail("The test case is a prototype.");
+    }
+
+    /**
+     * Test of convertK2M method, of class ConverterMethods.
+     */
+    @Test
+    public void testConvertK2M() {
+        System.out.println("convertK2M");
+        float base = 0.0F;
+        ConverterMethods instance = new ConverterMethods();
+        float expResult = 0.0F;
+        float result = instance.convertK2M(base);
+        assertEquals(expResult, result, 0.0);
+        // TODO review the generated test code and remove the default call to fail.
+        fail("The test case is a prototype.");
+    }
+
+    /**
+     * Test of convertG2L method, of class ConverterMethods.
+     */
+    @Test
+    public void testConvertG2L() {
+        System.out.println("convertG2L");
+        float base = 0.0F;
+        ConverterMethods instance = new ConverterMethods();
+        float expResult = 0.0F;
+        float result = instance.convertG2L(base);
+        assertEquals(expResult, result, 0.0);
+        // TODO review the generated test code and remove the default call to fail.
+        fail("The test case is a prototype.");
+    }
+
+    /**
+     * Test of convertL2G method, of class ConverterMethods.
+     */
+    @Test
+    public void testConvertL2G() {
+        System.out.println("convertL2G");
+        float base = 0.0F;
+        ConverterMethods instance = new ConverterMethods();
+        float expResult = 0.0F;
+        float result = instance.convertL2G(base);
+        assertEquals(expResult, result, 0.0);
+        // TODO review the generated test code and remove the default call to fail.
+        fail("The test case is a prototype.");
+    }
+
+    /**
+     * Test of convertOz2G method, of class ConverterMethods.
+     */
+    @Test
+    public void testConvertOz2G() {
+        System.out.println("convertOz2G");
+        float base = 0.0F;
+        ConverterMethods instance = new ConverterMethods();
+        float expResult = 0.0F;
+        float result = instance.convertOz2G(base);
+        assertEquals(expResult, result, 0.0);
+        // TODO review the generated test code and remove the default call to fail.
+        fail("The test case is a prototype.");
+    }
+
+    /**
+     * Test of convertG2Oz method, of class ConverterMethods.
+     */
+    @Test
+    public void testConvertG2Oz() {
+        System.out.println("convertG2Oz");
+        float base = 0.0F;
+        ConverterMethods instance = new ConverterMethods();
+        float expResult = 0.0F;
+        float result = instance.convertG2Oz(base);
+        assertEquals(expResult, result, 0.0);
+        // TODO review the generated test code and remove the default call to fail.
+        fail("The test case is a prototype.");
+    }
+
+    /**
+     * Test of convertLb2K method, of class ConverterMethods.
+     */
+    @Test
+    public void testConvertLb2K() {
+        System.out.println("convertLb2K");
+        float base = 0.0F;
+        ConverterMethods instance = new ConverterMethods();
+        float expResult = 0.0F;
+        float result = instance.convertLb2K(base);
+        assertEquals(expResult, result, 0.0);
+        // TODO review the generated test code and remove the default call to fail.
+        fail("The test case is a prototype.");
+    }
+
+    /**
+     * Test of convertK2Lb method, of class ConverterMethods.
+     */
+    @Test
+    public void testConvertK2Lb() {
+        System.out.println("convertK2Lb");
+        float base = 0.0F;
+        ConverterMethods instance = new ConverterMethods();
+        float expResult = 0.0F;
+        float result = instance.convertK2Lb(base);
+        assertEquals(expResult, result, 0.0);
+        // TODO review the generated test code and remove the default call to fail.
+        fail("The test case is a prototype.");
+    }
+    
+}
